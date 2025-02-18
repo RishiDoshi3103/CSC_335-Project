f5a086089708b0df53796ba48c58aa81d19e076a
Kyle Becker <kabecker@dhcp-10-134-155-231.uawifi.arizona.edu>
2025-02-18 12:05:02 -0700

    Base Song file

--- MusicLibraryProject/src/model/Song.java
+++ MusicLibraryProject/src/model/Song.java
@@ -1,5 +1,40 @@
+/*
+ *  This program is intended to represent individual songs present
+ *  in the database. Each song contains a title, album, and artist
+ *  associated with it.
+ *  
+ *  WIP - Consider adding a rating, maybe Enum class?
+ *  
+ *  Per LA1 specs:
+ *  - the ratings are 1 to 5
+ *	- songs do not have to be rated so there is no default rating
+ *	- songs that are rated as 5 should automatically be set to “favorite”
+ * 
+ */
+
 package model;
 
 public class Song {
-
+	private String title;
+	private String album;
+	private String artist;
+	// private Rating rating;
+	
+	public Song(String title, String album, String artist) {
+		this.title = title;
+		this.album = album;
+		this.artist = artist;
+	}
+	
+	public String getTitle() {
+		return this.title;
+	}
+	
+	public String getAlbum() {
+		return this.album;
+	}
+	
+	public String getArtist() {
+		return this.artist;
+	}
 }
