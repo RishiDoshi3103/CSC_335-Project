7ef8e550e38874c43fb9664a48c66c1222f48ace
Kyle Becker <kabecker@Save-the-Queen.local>
2025-02-21 11:42:27 -0700

    Branch creation, LibraryModel start

--- MusicLibraryProject/src/model/LibraryModel.java
+++ MusicLibraryProject/src/model/LibraryModel.java
@@ -1,5 +1,16 @@
+/*
+ * This program is meant to replicate the music library.
+ */
+
 package model;
 
+import java.util.ArrayList;
+
 public class LibraryModel {
+	private ArrayList<Album> library;
+	
+	public LibraryModel() {
+		this.library = new ArrayList<Album>();
+	}
 
 }
