b691f9699476cea4360972a835cfb6eae02ab00d
Rishi Doshi <rishidoshi60@gmail.com>
2025-02-23 19:22:51 -0700

    Add some comments on the code.
    Add some comments on the code.
    
    and made some changes in the Song.java class

--- MusicLibraryProject/src/MainPackage/Main.java
+++ MusicLibraryProject/src/MainPackage/Main.java
@@ -1,5 +1,5 @@
 package MainPackage;
 
 public class Main {
-
+	
 }
--- MusicLibraryProject/src/model/Album.java
+++ MusicLibraryProject/src/model/Album.java
@@ -3,14 +3,30 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The Album class represents a music album.
+ * It stores album metadata (title, artist, genre, year), an ordered list of song titles,
+ * an overall album rating, and a favorite flag.
+ */
+
 public class Album {
 	private String title;
 	private String artist;
 	private String genre;
 	private int year;
-	private List<String> songs;
+	private List<String> songs;		// List of song titles
 	private Rating rating;
 	private boolean favorite;
+	
+	/**
+     * Constructs a new Album with the given details.
+     * The album's initial rating is set to NOT_RATED and it is not marked as favorite.
+     *
+     * @param title  the album title
+     * @param artist the album artist
+     * @param genre  the genre of the album
+     * @param year   the release year
+     */
 	
 	public Album(String title, String artist, String genre, int year) {
 		this.title = title;
@@ -23,6 +39,12 @@ public class Album {
 		// Order for the Songs and the Artist are maintain hear.
 		this.songs = new ArrayList<>();
 	}
+	
+	/**
+     * Adds a song to the album while preserving the order.
+     *
+     * @param song the song title to add.
+     */
 	
 	public void addSong(String song) {
 		songs.add(song);
