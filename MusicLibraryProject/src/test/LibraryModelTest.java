eee461132655eae406dad6b5e3e26868b249f7ec
Kyle Becker <kabecker@dhcp-10-134-192-151.uawifi.arizona.edu>
2025-02-26 11:17:40 -0700

    Holding off on tests for now

--- MusicLibraryProject/src/test/LibraryModelTest.java
+++ MusicLibraryProject/src/test/LibraryModelTest.java
@@ -13,66 +13,66 @@ class LibraryModelTest {
 	@Test
 	void testGetSongTitle() {
 		MusicStore store = new MusicStore();
-		LibraryModel lib = new LibraryModel(store);
+		LibraryModel lib = new LibraryModel();
 	}
 	
 	@Test
 	void testGetSongByArtist() {
 		MusicStore store = new MusicStore();
-		LibraryModel lib = new LibraryModel(store);
+		LibraryModel lib = new LibraryModel();
 	}
 	
 	@Test
 	void testGetAlbumByTitle() {
 		MusicStore store = new MusicStore();
-		LibraryModel lib = new LibraryModel(store);
+		LibraryModel lib = new LibraryModel();
 	}
 	
 	@Test
 	void testGetAlbumByArtist() {
 		MusicStore store = new MusicStore();
-		LibraryModel lib = new LibraryModel(store);
+		LibraryModel lib = new LibraryModel();
 	}
 
 	@Test
 	void testGetPlayList() {
 		MusicStore store = new MusicStore();
-		LibraryModel lib = new LibraryModel(store);
+		LibraryModel lib = new LibraryModel();
 	}
 	
 	@Test
 	void testAddSong() {
 		MusicStore store = new MusicStore();
-		LibraryModel lib = new LibraryModel(store);
+		LibraryModel lib = new LibraryModel();
 	}
 	
 	@Test
 	void testAddAlbum() {
 		MusicStore store = new MusicStore();
-		LibraryModel lib = new LibraryModel(store);
+		LibraryModel lib = new LibraryModel();
 	}
 	
 	@Test
 	void testGetSongTitles() {
 		MusicStore store = new MusicStore();
-		LibraryModel lib = new LibraryModel(store);
+		LibraryModel lib = new LibraryModel();
 	}
 	
 	@Test
 	void testGetArtists() {
 		MusicStore store = new MusicStore();
-		LibraryModel lib = new LibraryModel(store);
+		LibraryModel lib = new LibraryModel();
 	}
 	
 	@Test
 	void testPlayLists() {
 		MusicStore store = new MusicStore();
-		LibraryModel lib = new LibraryModel(store);
+		LibraryModel lib = new LibraryModel();
 	}
 	
 	@Test
 	void testGetFavorites() {
 		MusicStore store = new MusicStore();
-		LibraryModel lib = new LibraryModel(store);
+		LibraryModel lib = new LibraryModel();
 	}
 }
